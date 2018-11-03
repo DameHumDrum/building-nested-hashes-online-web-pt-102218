cd963e71526ea5e1aef85944f661a31c95b34cea
Katie Ashlin <akka726@outlook.com>
2018-11-03 16:15:44 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,9 +1,9 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {:name => },
-      :matriarch => {:name => "Lady Montague", :age => "54"},
-      :hero => {:name => "Romeo", age = "15"},
+      :patriarch => {},
+      :matriarch => {,
+      :hero => {:name => },
       :hero_friends => [{}]
       }
       }, 
