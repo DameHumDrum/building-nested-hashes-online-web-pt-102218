e62a873c84235650ff0d33684beb8196db09df03
Katie Ashlin <akka726@outlook.com>
2018-11-03 16:11:20 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,15 +1,12 @@
 def first_challenge
    epic_tragedy = {
      :montague => {
-       :patriarch => {
-         :name =>
-       },
-      :matriarch => {
-        :name => 
-      },
-      :hero => {
-        :name =>
-      },
+      :patriarch => {:name => "Lord Montague", :age => "53"},
+      :matriarch => {:name => "Lady Montague", :age => "54"},
+      :hero => {:name => "Romeo", age = "15"},
+      :hero_friends => [
+          { :name => },
+        ]
       }
      },
      : capulat => {}
